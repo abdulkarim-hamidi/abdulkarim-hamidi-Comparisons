cb5d826e0ed346c6aff617535639d9cf174e1031
abdulkarim-hamidi <abdulkarim.hamidi@yahoo.com>
2023-01-31 22:12:29 +0000

    lab completed

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,13 @@
 public class Application {
     /**
-     * This class contains a main method that allows you to manually test the Comparisons lab functionality.
-     * No changes you make here affect the test case results. You must still run & pass the test cases.
+     * This class contains a main method that allows you to manually test 
+     * the Comparisons lab functionality.
+     * No changes you make here affect the test case results. You must still 
+     * run & pass the test cases.
      * All implementation code should be written in the Lab class.
      *
-     * The Lab methods like isEqual, notEqualTo, etc should produce true or false values depending on the numbers
+     * The Lab methods like isEqual, notEqualTo, etc should produce 
+     * true or false values depending on the numbers
      * provided.
      */
     public static void main(String[] args) {
